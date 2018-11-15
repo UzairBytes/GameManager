3b5a9cc59814cd9f8224c9963012028aef68d64f
joshua cerniglia <joshua.cerniglia@mail.utoronto.ca>
2018-11-15 17:16:19 -0500

    Made changes to settings activies

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/SlidingSettingsActivity.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/SlidingSettingsActivity.java
@@ -2,6 +2,10 @@ package fall2018.csc2017.slidingtiles;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SlidingSettingsActivity extends SettingsActivity {
     @Override
@@ -9,9 +13,53 @@ public class SlidingSettingsActivity extends SettingsActivity {
         super.onCreate(savedInstanceState);
     }
 
-    private void switchToGame(){
-        Intent start = new Intent(this, GameActivity.class);
+    @Override
+     void switchToGame(){
+        Intent start = new Intent(this, SlidingTilesGameActivity.class);
         saveToFile(SettingsActivity.TEMP_SAVE_FILENAME);
         startActivity(start);
     }
+
+    /**
+     * Activate Start button.
+     */
+    @Override
+     void addStartButtonListener(){
+        final Button start = findViewById(R.id.Start);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setSize();
+                setUndos();
+                if (size > 2 && size < 6) {
+                    switchToGame();
+                }
+                else {
+                    makeToastSize();
+                }
+            }
+        });
+    }
+
+    /**
+     * Displays that a size is not supported.
+     */
+    private void makeToastSize(){
+        Toast.makeText(this, "Please enter a size 3 to 5.", Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * Sets size from textView.
+     */
+    private void setSize(){
+        String strSize = ((EditText)findViewById(R.id.boardSizeInput)).getText().toString();
+        if(strSize.equals("")) {
+            size = 4;
+        }
+        else {
+            size = Integer.parseInt(strSize);
+        }
+        boardManager = new BoardManager(size);
+    }
 }
+
