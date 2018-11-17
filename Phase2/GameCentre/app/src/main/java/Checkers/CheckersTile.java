d6753ea8a5b6006b08bf180d486e8b03da21a77a
ChengJu3 <julian.cheng@mail.utoronto.ca>
2018-11-17 18:56:23 -0500

    cleaned CheckersTile

--- Phase2/GameCentre/app/src/main/java/Checkers/CheckersTile.java
+++ Phase2/GameCentre/app/src/main/java/Checkers/CheckersTile.java
@@ -1,6 +1,6 @@
 package Checkers;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 import fall2018.csc2017.slidingtiles.R;
 import fall2018.csc2017.slidingtiles.Tile;
@@ -9,44 +9,51 @@ import fall2018.csc2017.slidingtiles.Tile;
  * A Tile in checkers Game
  */
 public class CheckersTile extends Tile {
-    
+
+    /**
+     * Overwrite id to be a String
+     */
+    private String id;
+
     /**
      * Where it can take a piece
      */
-    boolean canTakePiece;
+    private boolean canTakePiece;
 
     /**
      * Arraylist of images
      */
-    ArrayList<Integer> picts = new ArrayList<>();
+    private HashMap<String, Integer> picts = new HashMap<>();
 
     /**
      * A checker tile with a background id
      */
-    public CheckersTile(int id) {
+    public CheckersTile(String id) {
         super();
         this.id = id;
         canTakePiece = false;
-
-        picts.add(R.drawable.checkerstile_0);
-        picts.add(R.drawable.checkerstile_1);
-        picts.add(R.drawable.checkerstile_2);
-        picts.add(R.drawable.checkerstile_3);
-        picts.add(R.drawable.checkerstile_4);
-        picts.add(R.drawable.checkerstile_5);
-        picts.add(R.drawable.checkerstile_6);
-        picts.add(R.drawable.checkerstile_7);
-        picts.add(R.drawable.checkerstile_8);
-        picts.add(R.drawable.checkerstile_9);
+        picts.put("empty_white_tile", R.drawable.checkers_empty_white_tile);
+        picts.put("empty_black_tile", R.drawable.checkers_empty_black_tile);
+        picts.put("red_pawn", R.drawable.checkers_red_pawn);
+        picts.put("red_king", R.drawable.checkers_red_king);
+        picts.put("white_pawn", R.drawable.checkers_white_pawn);
+        picts.put("white_king", R.drawable.checkers_white_king);
+        picts.put("red_pawn_highlighted", R.drawable.checkers_red_pawn_highlighted);
+        picts.put("red_king_highlighted", R.drawable.checkers_red_king_highlighted);
+        picts.put("white_pawn_highlighted", R.drawable.checkers_white_pawn_highlighted);
+        picts.put("white_king_highlighted", R.drawable.checkers_white_king_highlighted);
         background = picts.get(id);
 
     }
 
     /**
-     * Makes the piece a king by incriminating its id by 1
+     * Makes the piece a king
      */
     protected void makeKing(){
-        id++;
+        if (this.id.contains("white")){
+            this.id = "white_king";
+        }
+        else {this.id = "red_king";}
         background = picts.get(id);
     }
 
@@ -69,12 +76,11 @@ public class CheckersTile extends Tile {
     }
     
     public void highlight() {
-        id += 4;
+        this.id += "_highlighted";
         background = picts.get(id);
     }
     
     public void dehighlight() {
-        id -= 4;
-        background = picts.get(id);
+        String newId = this.id.replaceAll("_highlighted$","");
     }
 }
