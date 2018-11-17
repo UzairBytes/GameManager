5c5edad36f6c666b06e6f2bea639d4536bd105b5
joshua cerniglia <joshua.cerniglia@mail.utoronto.ca>
2018-11-17 16:15:39 -0500

    Added basic methods to CheckersTile

--- Phase2/GameCentre/app/src/main/java/Checkers/CheckersTile.java
+++ Phase2/GameCentre/app/src/main/java/Checkers/CheckersTile.java
@@ -3,9 +3,27 @@ package Checkers;
 import fall2018.csc2017.slidingtiles.Tile;
 
 public class CheckersTile extends Tile {
+
+    int backgroundId;
+    
+    boolean canTakePiece;
     /**
      * A tile with a background id; look up and set the id.
      */
-    CheckersTile() {
+    public CheckersTile(int backgroundId) {
+        super();
+        this.backgroundId = backgroundId;
+        canTakePiece = false;
+    }
+    protected void makeKing(){
+        backgroundId++;
+    }
+
+    public void setCanTakePiece(boolean canTakePiece){
+        this.canTakePiece = canTakePiece;
+    }
+
+    public boolean isCanTakePiece() {
+        return canTakePiece;
     }
 }
