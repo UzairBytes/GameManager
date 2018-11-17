75b9ae6eab4797337d7c4b0b37f6037e9d8b35b5
joshua cerniglia <joshua.cerniglia@mail.utoronto.ca>
2018-11-17 16:59:12 -0500

    Finishedish CheckersTile

--- Phase2/GameCentre/app/src/main/java/Checkers/CheckersTile.java
+++ Phase2/GameCentre/app/src/main/java/Checkers/CheckersTile.java
@@ -8,32 +8,45 @@ import fall2018.csc2017.slidingtiles.Tile;
  * A Tile in checkers Game
  */
 public class CheckersTile extends Tile {
-
-    /**
-     * The background id to find the tile image.
-     */
-    int backgroundId;
-
+    
     /**
      * Where it can take a piece
      */
     boolean canTakePiece;
 
     /**
+     * Arraylist of images
+     */
+    ArrayList<Integer> picts = new ArrayList<>();
+
+    /**
      * A checker tile with a background id
      */
-    public CheckersTile(int backgroundId) {
+    public CheckersTile(int id) {
         super();
-        this.backgroundId = backgroundId;
+        this.id = id;
         canTakePiece = false;
-        ArrayList<Integer> picts = new ArrayList<>();
+        /*
+        picts.add(R.drawable.checkerstile_0);
+        picts.add(R.drawable.checkerstile_1);
+        picts.add(R.drawable.checkerstile_2);
+        picts.add(R.drawable.checkerstile_3);
+        picts.add(R.drawable.checkerstile_4);
+        picts.add(R.drawable.checkerstile_5);
+        picts.add(R.drawable.checkerstile_6);
+        picts.add(R.drawable.checkerstile_7);
+        picts.add(R.drawable.checkerstile_8);
+        picts.add(R.drawable.checkerstile_9);
+        background = picts.get(id);
+         */
     }
 
     /**
-     * Makes the piece a king by incriminating its backgroundId by 1
+     * Makes the piece a king by incriminating its id by 1
      */
     protected void makeKing(){
-        backgroundId++;
+        id++;
+        background = picts.get(id);
     }
 
     /**
@@ -52,5 +65,15 @@ public class CheckersTile extends Tile {
      */
     public boolean isCanTakePiece() {
         return canTakePiece;
+    }
+    
+    public void highlight() {
+        id += 4;
+        background = picts.get(id);
+    }
+    
+    public void dehighlight() {
+        id -= 4;
+        background = picts.get(id);
     }
 }
