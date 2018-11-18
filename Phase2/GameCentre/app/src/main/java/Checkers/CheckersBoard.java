7ec16e362c76e1c10fc98753238f60784c05cf19
joshua cerniglia <joshua.cerniglia@mail.utoronto.ca>
2018-11-18 00:38:24 -0500

    Changed id reference to the new string ids.

--- Phase2/GameCentre/app/src/main/java/Checkers/CheckersBoard.java
+++ Phase2/GameCentre/app/src/main/java/Checkers/CheckersBoard.java
@@ -16,19 +16,19 @@ public class CheckersBoard extends Board {
         for (int row = 0; row < 8; row++){
             for (int col = 0; col < 8; col++){
                 CheckersTile curtile = tiles[row][col];
-                int id = curtile.getId();
-                if(id == 2 ){
+                String id = curtile.getId();
+                if(id.equals("red_pawn")){
                     curtile.setCanTakePiece(redPawnCanTake(row, col));
                 }
 
-                if(id == 3 ){
+                if(id.equals("red_king")){
                     curtile.setCanTakePiece(redKingCanTake(row, col));
                 }
 
-                if(id == 4 ){
+                if(id.equals("white_pawn")){
                     curtile.setCanTakePiece(whitePawnCanTake(row, col));
                 }
-                if(id == 5 ){
+                if(id.equals("white_king")){
                     curtile.setCanTakePiece(whiteKingCanTake(row, col));
                 }
             }
@@ -38,10 +38,10 @@ public class CheckersBoard extends Board {
     private boolean redPawnCanTake(int row, int col){
         boolean canTake = false;
         if (emptyTopRight(row, col)){
-            canTake = canTake || tiles[row - 1][col - 1].getId() == 4 || tiles[row - 1][col - 1].getId() == 5;
+            canTake = canTake || tiles[row - 1][col - 1].getId().equals("white_pawn") || tiles[row - 1][col - 1].getId().equals("white_king");
         }
         if (emptyTopLeft(row, col)){
-            canTake = canTake || tiles[row - 1][col + 1].getId() == 4 || tiles[row - 1][col + 1].getId() == 5;
+            canTake = canTake || tiles[row - 1][col + 1].getId().equals("white_pawn") || tiles[row - 1][col + 1].getId().equals("white_king");
         }
         return canTake;
     }
@@ -49,10 +49,10 @@ public class CheckersBoard extends Board {
     private boolean whitePawnCanTake(int row, int col){
         boolean canTake = false;
         if (emptyBottomRight(row, col)){
-            canTake = canTake || tiles[row + 1][col - 1].getId() == 2 || tiles[row + 1][col - 1].getId() == 3;
+            canTake = canTake || tiles[row + 1][col - 1].getId().equals("red_pawn") || tiles[row + 1][col - 1].getId().equals("red_king");
         }
         if (emptyBottomLeft(row, col)){
-            canTake = canTake || tiles[row + 1][col + 1].getId() == 2 || tiles[row + 1][col + 1].getId() == 3;
+            canTake = canTake || tiles[row + 1][col + 1].getId().equals("red_pawn") || tiles[row + 1][col + 1].getId().equals("red_king");
         }
         return canTake;
     }
@@ -60,10 +60,10 @@ public class CheckersBoard extends Board {
     private boolean redKingCanTake(int row, int col){
         boolean canTake = redPawnCanTake(row, col);
         if (emptyBottomRight(row, col)){
-            canTake = canTake || tiles[row + 1][col - 1].getId() == 4 || tiles[row + 1][col - 1].getId() == 5;
+            canTake = canTake || tiles[row + 1][col - 1].getId().equals("white_pawn") || tiles[row + 1][col - 1].getId().equals("white_king");
         }
         if (emptyBottomLeft(row, col)){
-            canTake = canTake || tiles[row + 1][col + 1].getId() == 4 || tiles[row + 1][col + 1].getId() == 5;
+            canTake = canTake || tiles[row + 1][col + 1].getId().equals("white_pawn") || tiles[row + 1][col + 1].getId().equals("white_king");
         }
         return canTake;
     }
@@ -71,10 +71,10 @@ public class CheckersBoard extends Board {
     private boolean whiteKingCanTake(int row, int col){
         boolean canTake = whitePawnCanTake(row, col);
         if (emptyTopRight(row, col)){
-            canTake = canTake || tiles[row - 1][col - 1].getId() == 2 || tiles[row - 1][col - 1].getId() == 3;
+            canTake = canTake || tiles[row - 1][col - 1].getId().equals("red_pawn") || tiles[row - 1][col - 1].getId().equals("red_king");
         }
         if (emptyTopLeft(row, col)){
-            canTake = canTake || tiles[row - 1][col + 1].getId() == 2 || tiles[row - 1][col + 1].getId() == 3;
+            canTake = canTake || tiles[row - 1][col + 1].getId().equals("red_pawn") || tiles[row - 1][col + 1].getId().equals("red_king");
         }
         return canTake;
     }
@@ -83,27 +83,27 @@ public class CheckersBoard extends Board {
         if (col < 2 || row < 2) {
             return false;
         }
-        return tiles[row - 2][col - 2].getId() == 1;
+        return tiles[row - 2][col - 2].getId().equals("empty_black_tile");
     }
 
     private boolean emptyTopRight(int row, int col){
         if (col > 5 || row < 2) {
             return false;
         }
-        return tiles[row - 2][col + 2].getId() == 1;
+        return tiles[row - 2][col + 2].getId().equals("empty_black_tile");
     }
 
     private boolean emptyBottomLeft(int row, int col){
         if (col < 2 || row > 5) {
             return false;
         }
-        return tiles[row + 2][col - 2].getId() == 1;
+        return tiles[row + 2][col - 2].getId().equals("empty_black_tile");
     }
 
     private boolean emptyBottomRight(int row, int col){
         if (col > 5 || row > 5) {
             return false;
         }
-        return tiles[row + 2][col + 2].getId() == 1;
+        return tiles[row + 2][col + 2].getId().equals("empty_black_tile");
     }
 }
