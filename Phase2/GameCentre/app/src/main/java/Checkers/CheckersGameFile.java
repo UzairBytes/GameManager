f82c40bf2f8ab3e8892d6eb02669eaebab4bf8c1
joshua cerniglia <joshua.cerniglia@mail.utoronto.ca>
2018-11-18 18:17:46 -0500

    More fully implemented CheckerGameFile

--- Phase2/GameCentre/app/src/main/java/Checkers/CheckersGameFile.java
+++ Phase2/GameCentre/app/src/main/java/Checkers/CheckersGameFile.java
@@ -3,4 +3,35 @@ package Checkers;
 import phase1.GameFile;
 
 public class CheckersGameFile extends GameFile {
+
+    /**
+     * Maximum number of undos for a set of SlidingGameFile.
+     */
+    public int maxUndos;
+
+    /**
+     * Number of moves this user has made.
+     */
+    public int numMoves;
+
+    /**
+     * Undos remaining for a set of SlidingGameFile.
+     */
+    public int remainingUndos;
+
+    /**
+     * Constructor called when creating a new game, with it's specified initial game state and game name.
+     *
+     * @param initialBoard: The initial state of the SlidingTiles board
+     * @param name:         The name of this game file
+     */
+    @SuppressWarnings("unchecked")
+    public CheckersGameFile(CheckersBoard initialBoard, String name) {
+        this.gameStates.push(initialBoard);
+        this.name = name;
+    }
+
+    public void increaseNumMoves(){
+        numMoves++;
+    }
 }
