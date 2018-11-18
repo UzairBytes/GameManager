824e5ec56ae26be58baa92381d10fb5ebacdf306
ChengJu3 <julian.cheng@mail.utoronto.ca>
2018-11-17 22:24:54 -0500

    added a few tests for CheckersTile

--- Phase2/GameCentre/app/src/test/java/GameCentre/CheckersTileTest.java
+++ Phase2/GameCentre/app/src/test/java/GameCentre/CheckersTileTest.java
@@ -1,6 +1,22 @@
 package GameCentre;
 
+import org.junit.Test;
 import Checkers.CheckersTile;
+import static org.junit.Assert.assertEquals;
 
-class CheckersTileTest {
+public class CheckersTileTest {
+
+    @Test
+    public void testHighlightDehighlightTile(){
+        CheckersTile redKing = new CheckersTile("red_king");
+        CheckersTile emptyWhite = new CheckersTile("empty_white_tile");
+        CheckersTile redPawn = new CheckersTile("red_pawn");
+        CheckersTile redPawnHighlighted = new CheckersTile("red_pawn_highlighted");
+
+        redPawn.highlight();
+        assertEquals("CheckersTile.highlight() failed", redPawn.getId(), redPawnHighlighted.getId());
+        redPawn.dehighlight();
+        assertEquals("CheckersTile.dehighlight() failed", redPawn.getId(), new CheckersTile("red_pawn").getId());
+
+    }
 }
