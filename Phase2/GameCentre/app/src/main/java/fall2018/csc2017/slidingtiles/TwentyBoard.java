0ed1fff42cd14b6ef83dc9e5ab8894f57e9c7fa2
gupta-sid <sid.gupta@mail.utoronto.ca>
2018-11-19 13:29:21 -0500

    Implemented mergeTile method in the TwentyBoard class. Also refactored generatedRandomTile method in TwentyBoard.

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/TwentyBoard.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/TwentyBoard.java
@@ -3,14 +3,30 @@ import java.util.ArrayList;
 
 public class TwentyBoard extends Board {
 
-    public void swap(int pos1, int pos2){
+    /*
+     * @param row1 the first tile row
+     * @param col1 the first tile col
+     * @param row2 the second tile row
+     * @param col2 the second tile col
+     * preconditions: the tile at row1, col1 has the same id as the tile at row2, col2
+     */
+    public TwentyTile mergeTiles(int row1, int col1, int row2, int col2){
+        int tile1Id = this.tiles[row1][col1].id, tile2Id = this.tiles[row1][col1].id;
 
+        // Verify preconditions.
+        if(tile1Id == tile2Id){
+            // TODO: Insert a real background image
+            return new TwentyTile(tile1Id*tile2Id, tile1Id*tile2Id);
+        }else{
+            // Return null if preconditions are not met.
+            return null;
+        }
     }
 
-    /* Generate a random tile at a given position. */
-    public void generateRandomTile(int position[]){
-        int tileRow = position[0];
-        int tileCol = position[1];
+    /* Generate a random tile in place of an empty tile in the Board. */
+    public void generateRandomTile(){
+        int position[] = getRanEmptyPos();
+        int tileRow = position[0], tileCol = position[1];
 
         // Pick a random value from (2^1, 2^2, 2^3)
         int ranExp = (int)(Math.random() * 4 + 1);
@@ -24,7 +40,7 @@ public class TwentyBoard extends Board {
     }
 
     /* Get the positions of a random empty tile in this Board. */
-    public int[] getRanEmptyPos(){
+    private int[] getRanEmptyPos(){
         int[] ranEmptyPos = new int[2];
         ArrayList<int[]> emptyPositions = getAllEmptyPos();
         if(emptyPositions.isEmpty()){
@@ -39,7 +55,7 @@ public class TwentyBoard extends Board {
     }
 
     /* Get the positions of all the empty tiles in this Board. */
-    public ArrayList<int[]> getAllEmptyPos(){
+    private ArrayList<int[]> getAllEmptyPos(){
         ArrayList<int[]> emptyPositions = new ArrayList<>();
         // Iterate through this TwentyBoard to find & retrieve the position of all the empty tiles
         TwentyTile currentTile;
