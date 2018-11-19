f35d7b453d356db0bdb5b4821d3340be8d2b8350
gupta-sid <sid.gupta@mail.utoronto.ca>
2018-11-19 13:35:53 -0500

    Created instance variables in TwentyBoardManager

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/TwentyBoardManager.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/TwentyBoardManager.java
@@ -1,4 +1,32 @@
 package fall2018.csc2017.slidingtiles;
 
-public class TwentyBoardManager {
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import phase1.AccountManager;
+import phase1.SlidingGameFile;
+
+public class TwentyBoardManager extends BoardManager {
+
+
+    /**
+     * The board being managed.
+     */
+    protected TwentyBoard twentyBoard;
+
+    /**
+     * The 2048 Game File holding the data for this board.
+     */
+    private SlidingGameFile gameFile;
+
+    public TwentyBoardManager(int size) {
+
+    }
+
+    public void touchMove(){
+
+    }
+
 }
