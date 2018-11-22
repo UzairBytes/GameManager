763c6da49dd22aeb2eb7582b877a6791180db736
Uzair Chudhary <uzair.chudhary@mail.utoronto.ca>
2018-11-22 15:10:16 -0500

    TwentyGameActivity class

---
+++ Phase2/GameCentre/app/src/main/java/Twenty/TwentyGameActivity.java
@@ -0,0 +1,17 @@
+package Twenty;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fall2018.csc2017.CoreClasses.GameActivity;
+import fall2018.csc2017.CoreClasses.R;
+
+
+public class TwentyGameActivity extends GameActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_twenty_game);
+    }
+}
