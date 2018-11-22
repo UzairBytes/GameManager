d285a1cda56301a135a5cee29767742c63f4a31b
Uzair Chudhary <uzair.chudhary@mail.utoronto.ca>
2018-11-22 15:44:45 -0500

    Modified OnCreate method for TwentyGameActivity

--- Phase2/GameCentre/app/src/main/java/Twenty/TwentyGameActivity.java
+++ Phase2/GameCentre/app/src/main/java/Twenty/TwentyGameActivity.java
@@ -2,6 +2,14 @@ package Twenty;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.Button;
+
+import java.util.Observable;
+import java.util.Observer;
 
 import fall2018.csc2017.CoreClasses.GameActivity;
 import fall2018.csc2017.CoreClasses.R;
@@ -9,9 +17,22 @@ import fall2018.csc2017.CoreClasses.R;
 
 public class TwentyGameActivity extends GameActivity {
 
+    private TwentyBoardManager boardManager;
+
+    private TwentyGestureDetectGridView gridView;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        loadFromFile();
         setContentView(R.layout.activity_twenty_game);
+
+        gridView = findViewById(R.id.gridTwenty);
+        gridView.setNumColumns(boardManager.getSize());
+        gridView.setBoardManager(boardManager);
+
     }
+
 }
