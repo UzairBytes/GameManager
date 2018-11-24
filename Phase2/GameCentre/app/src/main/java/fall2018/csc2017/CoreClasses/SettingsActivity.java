7020f264ef35e8702ff02cfecba290711e6d7db1
joshua cerniglia <joshua.cerniglia@mail.utoronto.ca>
2018-11-24 17:24:08 -0500

    Removed saving from parent class

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/CoreClasses/SettingsActivity.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/CoreClasses/SettingsActivity.java
@@ -3,27 +3,13 @@ package fall2018.csc2017.CoreClasses;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.EditText;
 
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-
-import Checkers.CheckersBoardManager;
-import Sliding.SlidingBoardManager;
-import Twenty.TwentyBoardManager;
-import phase1.AccountManager;
-import phase1.Game;
 
 /**
  * The activity for determining settings for a new game.
  */
 public class SettingsActivity extends AppCompatActivity {
-
-    /**
-     * The board manager.
-     */
-     protected BoardManager boardManager;
 
     /**
      * A temporary save file.
@@ -41,7 +27,6 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        saveToFile(TEMP_SAVE_FILENAME);
         setContentView(R.layout.activity_settings);
         EditText undosInput = findViewById(R.id.undosInput);
         undosInput.setText("3");
@@ -61,22 +46,6 @@ public class SettingsActivity extends AppCompatActivity {
      * Activate Start button.
      */
     void addStartButtonListener(){
-    }
-
-    /**
-     * Save the board manager to fileName.
-     *
-     * @param fileName the name of the file
-     */
-    public void saveToFile(String fileName) {
-        try {
-            ObjectOutputStream outputStream = new ObjectOutputStream(
-                    this.openFileOutput(fileName, MODE_PRIVATE));
-            outputStream.writeObject(boardManager);
-            outputStream.close();
-        } catch (IOException e) {
-            Log.e("Exception", "File write failed: " + e.toString());
-        }
     }
 
     /**
