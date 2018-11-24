7b60dc4d63400b9544f6d07ffae4c5d59ccb470a
gabejona <jonathan.gabe@mail.utoronto.ca>
2018-11-24 18:56:53 -0500

    Corrected inheritance of methods in SlidingBoard.

--- Phase2/GameCentre/app/src/main/java/Sliding/SlidingBoard.java
+++ Phase2/GameCentre/app/src/main/java/Sliding/SlidingBoard.java
@@ -7,61 +7,62 @@ import java.util.Iterator;
 import java.util.List;
 
 import fall2018.csc2017.CoreClasses.Board;
+import fall2018.csc2017.CoreClasses.Tile;
 
 /**
  * The sliding tiles board.
  */
-public class SlidingBoard extends Board implements Iterable<SlidingTile> {
+public class SlidingBoard extends Board {
 
-    /**
-     * The tiles on the board in row-major order.
-     */
-    private SlidingTile[][] tiles;
+//    /**
+//     * The tiles on the board in row-major order.
+//     */
+//    private SlidingTile[][] tiles;
+//
+//    /**
+//     * Stores the list of SlidingTile objects when this Board is initialized.
+//     */
+//    private List<SlidingTile> tilesList;
 
-    /**
-     * Stores the list of SlidingTile objects when this Board is initialized.
-     */
-    private List<SlidingTile> tilesList;
 
-
-    /**
-     * Stores the list of SlidingTile Iterator of this Board.
-     */
-    @NonNull
-    @Override
-    public Iterator<SlidingTile> iterator() {
-        return new SlidingTileIterator();
-    }
-
-    /**
-     * The tile iterator, iterates through the tiles on the board.
-     */
-    class SlidingTileIterator implements Iterator<SlidingTile> {
-        /**
-         * The column of the next Tile.
-         */
-        private int nextColumn = 0;
-        /**
-         * The row of the next Tile.
-         */
-        private int nextRow = 0;
-
-        @Override
-        public SlidingTile next() {
-            SlidingTile nextTile = tiles[nextRow][nextColumn];
-            nextColumn += 1;
-            if (nextColumn == getNumCols()) {
-                nextColumn = 0;
-                nextRow += 1;
-            }
-            return nextTile;
-        }
-
-        @Override
-        public boolean hasNext() {
-            return (nextRow < getNumRows());
-        }
-    }
+//    /**
+//     * Stores the list of SlidingTile Iterator of this Board.
+//     */
+//    @NonNull
+//    @Override
+//    public Iterator<SlidingTile> iterator() {
+//        return new SlidingTileIterator();
+//    }
+//
+//    /**
+//     * The tile iterator, iterates through the tiles on the board.
+//     */
+//    class SlidingTileIterator implements Iterator<SlidingTile> {
+//        /**
+//         * The column of the next Tile.
+//         */
+//        private int nextColumn = 0;
+//        /**
+//         * The row of the next Tile.
+//         */
+//        private int nextRow = 0;
+//
+//        @Override
+//        public SlidingTile next() {
+//            SlidingTile nextTile = tiles[nextRow][nextColumn];
+//            nextColumn += 1;
+//            if (nextColumn == getNumCols()) {
+//                nextColumn = 0;
+//                nextRow += 1;
+//            }
+//            return nextTile;
+//        }
+//
+//        @Override
+//        public boolean hasNext() {
+//            return (nextRow < getNumRows());
+//        }
+//    }
 
     /**
      * A new board of tiles in row-major order.
@@ -70,73 +71,56 @@ public class SlidingBoard extends Board implements Iterable<SlidingTile> {
      * @param tiles the tiles for the board
      */
 
-    public SlidingBoard(List<SlidingTile> tiles, int rows, int cols) {
-        setNumRows(rows);
-        setNumCols(cols);
-        this.tiles = new SlidingTile[getNumRows()][getNumCols()];
-        this.tilesList = tiles;
-        Iterator<SlidingTile> iter = tiles.iterator();
-
-        for (int row = 0; row != getNumRows(); row++) {
-            for (int col = 0; col != getNumCols(); col++) {
-                this.tiles[row][col] = iter.next();
-            }
-        }
+    public SlidingBoard(Tile[][] tiles, int rows, int cols) {
+        super(tiles, rows, cols);
     }
 
     /**
      * Returns a deep-copy of this Board.
      */
-    SlidingBoard createDeepCopy() {
-        List<SlidingTile> copiedTiles = new ArrayList<>(this.tilesList);
-
-        SlidingTile[][] copyTile = new SlidingTile[getNumRows()][getNumCols()];
-        for (int row = 0; row != getNumRows(); row++) {
-            for (int col = 0; col != getNumCols(); col++) {
-                copyTile[row][col] = this.tiles[row][col];
-            }
-        }
-        SlidingBoard copiedBoard = new SlidingBoard(copiedTiles, getNumRows(), getNumCols());
-        copiedBoard.tiles = copyTile;
+    @Override
+    public SlidingBoard createDeepCopy() {
+        Tile[][] tiles = super.createDeepCopy().tiles;
+        SlidingBoard copiedBoard = new SlidingBoard(tiles, getNumRows(), getNumCols());
         return copiedBoard;
     }
 
-    /**
-     * Return the tile at (row, col)
-     *
-     * @param row the tile row
-     * @param col the tile column
-     * @return the tile at (row, col)
-     */
-    SlidingTile getSlidingTile(int row, int col) {
-        return tiles[row][col];
-    }
+//    /**
+//     * Return the tile at (row, col)
+//     *
+//     * @param row the tile row
+//     * @param col the tile column
+//     * @return the tile at (row, col)
+//     */
+//    SlidingTile getSlidingTile(int row, int col) {
+//        return tiles[row][col];
+//    }
 
-    /**
-     * Swap the tiles at (row1, col1) and (row2, col2)
-     *
-     * @param row1 the first tile row
-     * @param col1 the first tile col
-     * @param row2 the second tile row
-     * @param col2 the second tile col
-     */
-    public void swapSlidingTiles(int row1, int col1, int row2, int col2) {
-
-        SlidingTile[][] holdTile = new SlidingTile[1][2];
-        holdTile[0][0] = getSlidingTile(row1, col1);
-        holdTile[0][1] = getSlidingTile(row2, col2);
-        this.tiles[row2][col2] = holdTile[0][0];
-        this.tiles[row1][col1] = holdTile[0][1];
-    }
-
-    /**
-     * Returns tilesList.
-     *
-     * @return list of tiles.
-     */
-    public List<SlidingTile> getTilesList(){
-        return this.tilesList;
-    }
+//    /**
+//     * Swap the tiles at (row1, col1) and (row2, col2)
+//     *
+//     * @param row1 the first tile row
+//     * @param col1 the first tile col
+//     * @param row2 the second tile row
+//     * @param col2 the second tile col
+//     */
+//    public void swapSlidingTiles(int row1, int col1, int row2, int col2) {
+//
+//        SlidingTile[][] holdTile = new SlidingTile[1][2];
+//        holdTile[0][0] = getSlidingTile(row1, col1);
+//        holdTile[0][1] = getSlidingTile(row2, col2);
+//        this.tiles[row2][col2] = holdTile[0][0];
+//        this.tiles[row1][col1] = holdTile[0][1];
+//    }
+//
+//    /**
+//     * Returns tilesList.
+//     *
+//     * @return list of tiles.
+//     */
+//    public List<SlidingTile> getTilesList(){
+//        return this.tilesList;
+//    }
 
 
 
