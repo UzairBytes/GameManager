939923673f358da493dfc048062428d5b0729ddf
Uzair Chudhary <uzair.chudhary@mail.utoronto.ca>
2018-11-26 16:24:22 -0500

    Modified GameActivity to set the new Adapter and implement Observer

--- Phase2/GameCentre/app/src/main/java/Twenty/TwentyGameActivity.java
+++ Phase2/GameCentre/app/src/main/java/Twenty/TwentyGameActivity.java
@@ -8,30 +8,69 @@ import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.Button;
 
+import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
 
+import fall2018.csc2017.CoreClasses.CustomAdapter;
 import fall2018.csc2017.CoreClasses.GameActivity;
 import fall2018.csc2017.CoreClasses.R;
 
 
-public class TwentyGameActivity extends GameActivity {
+public class TwentyGameActivity extends GameActivity implements Observer {
 
     private TwentyBoardManager boardManager;
 
     private TwentyGestureDetectGridView gridView;
 
+    private static int columnWidth, columnHeight;
 
+    private ArrayList<Button> tileButtons;
+
+    public void display() {
+        updateTileButtons();
+        gridView.setAdapter(new CustomAdapter(tileButtons, columnWidth, columnHeight));
+    }
+
+    private void createTileButtons(Context context){
+        TwentyBoard board = boardManager.getBoard();
+        tileButtons = new ArrayList<>();
+        for (int row = 0; row != board.getNumRows(); row++) {
+            for (int col = 0; col != board.getNumCols(); col++) {
+                Button tmp = new Button(context);
+                tmp.setBackgroundResource(board.getTile(row, col).getBackground());
+                this.tileButtons.add(tmp);
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         loadFromFile();
+        createTileButtons(this);
         setContentView(R.layout.activity_twenty_game);
 
         gridView = findViewById(R.id.gridTwenty);
         gridView.setNumColumns(boardManager.getSize());
         gridView.setBoardManager(boardManager);
+        boardManager.addObserver(this);
+
+        gridView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                gridView.getViewTreeObserver().removeOnGlobalLayoutListener(
+                        this);
+                int displayWidth = gridView.getMeasuredWidth();
+                int displayHeight = gridView.getMeasuredHeight();
+
+                columnWidth = displayWidth / boardManager.twentyBoard.getNumCols();
+                columnHeight = displayHeight / boardManager.twentyBoard.getNumCols();
+
+                display();
+            }
+        });
+
         addUndoButtonListener();
     }
 
@@ -45,4 +84,19 @@ public class TwentyGameActivity extends GameActivity {
         });
     }
 
+    @Override
+    public void update(Observable o, Object arg) {
+        display();
+    }
+
+    private void updateTileButtons() {
+        TwentyBoard board = boardManager.getBoard();
+        int nextPos = 0;
+        for (Button b : tileButtons) {
+            int row = nextPos / board.getNumCols();
+            int col = nextPos % board.getNumCols();
+            b.setBackgroundResource(board.getTile(row, col).getBackground());
+            nextPos++;
+        }
+    }
 }
