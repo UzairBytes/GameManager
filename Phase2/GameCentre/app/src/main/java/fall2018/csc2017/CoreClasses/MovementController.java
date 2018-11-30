07d852c7bee1a247449197c68c08942f7a47f679
ChengJu3 <julian.cheng@mail.utoronto.ca>
2018-11-30 00:09:47 -0500

    changed gameName used to updateScores

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/CoreClasses/MovementController.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/CoreClasses/MovementController.java
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
 import phase1.AccountManager;
-import phase1.Game;
 import phase1.GameScore;
 import phase1.LeaderBoard;
 
@@ -27,7 +26,7 @@ public class MovementController extends AppCompatActivity {
         AccountManager.activeAccount.addGameFile(boardManager.getGameFile());
         if (boardManager.gameComplete()){
             LeaderBoard.updateScores(new GameScore(
-                    Game.CHECKERS_NAME, boardManager.getGameFile().getName(),
+                    AccountManager.activeAccount.getActiveGameName(), boardManager.getGameFile().getName(),
                     AccountManager.activeAccount.getUsername(), boardManager.score()));
             Toast.makeText(context, toastMessage, Toast.LENGTH_SHORT).show();
         }
