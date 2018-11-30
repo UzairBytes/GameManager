4352a7228a302a2c8368e4e91f3f5a8cc79b55d4
Uzair Chudhary <uzair.chudhary@mail.utoronto.ca>
2018-11-30 10:41:04 -0500

    Added javadoc

--- Phase2/GameCentre/app/src/main/java/Checkers/CheckersGestureDetectGridView.java
+++ Phase2/GameCentre/app/src/main/java/Checkers/CheckersGestureDetectGridView.java
@@ -11,14 +11,30 @@ import fall2018.csc2017.CoreClasses.GestureDetectGridView;
 
 public class CheckersGestureDetectGridView extends GestureDetectGridView {
 
+    /**
+     * Calls the constructor to create a CheckersGestureDetectGridView when an attribute of it is set.
+     * @param context
+     * @param attrs
+     */
     public CheckersGestureDetectGridView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
+    /**
+     * Calls the constructor to create a CheckersGestureDetectGridView when it is called in with context.
+     * @param context
+     */
     public CheckersGestureDetectGridView(Context context) {
         super(context);
     }
 
+    /**
+     * Calls the constructor to create a CheckersGestureDetectGridView when an attribute and
+     * style attribute is set.
+     * @param context
+     * @param attrs
+     * @param defStyleAttr
+     */
     public CheckersGestureDetectGridView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
